0405ca6a4de9139cd88faa0f6f42a2dc310d24e2
Aayash <dangaayash@gmail.com>
2026-05-22 13:26:27 +0545

    multer config updated with videos

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,10 +3,13 @@ import multer from 'multer'
 const storage = multer.memoryStorage()
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) {
+  if (
+    file.mimetype.startsWith('image/') ||
+    file.mimetype.startsWith('video/')
+  ) {
     cb(null, true)
   } else {
-    cb(new Error('Only image files are allowed'), false)
+    cb(new Error('Only image and video files are allowed'), false)
   }
 }
 
@@ -14,7 +17,7 @@ const upload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 2 * 1024 * 1024, 
+    fileSize: 500 * 1024 * 1024, 
   },
 })
 
