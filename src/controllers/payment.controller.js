8fd32342a0c2162f3b15cc74da08d77ea6c05284
Aayash <dangaayash@gmail.com>
2026-05-28 10:51:48 +0545

    fixed payment controller for deployment

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -8,11 +8,10 @@ export const initiate = asyncHandler(async (req, res) => {
 })
 
 export const verify = asyncHandler(async (req, res) => {
-  const { data: encodedData, courseId } = req.query
-  console.log('req.user._id:', req.user._id)  
+  const { data: encodedData } = req.query
   if (!encodedData) {
     return errorResponse(res, { message: 'Missing payment data', statusCode: 400 })
   }
-  const result = await verifyPayment(req.user._id, encodedData)
+  const result = await verifyPayment(encodedData)  // no req.user._id needed
   return successResponse(res, { message: 'Payment verified and enrollment activated', data: result })
 })
